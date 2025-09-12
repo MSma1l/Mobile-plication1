5963b34025ad495f8e5cd41173c588b43ce803bb
Maxim <maxim.chistol@iis.utm.md>
2025-09-12 12:16:46 +0300

    Init project

---
+++ Outfitly/App.tsx
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Chistol Maxim,Casico Anna,Gasca Vlad,Ionescu Valentin Grupa IA-231</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
